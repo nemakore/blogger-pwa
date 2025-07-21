1013e587756823fe7b8c4803cbfa90b16c14e0b7
Nema Kore <156081686+nemakore@users.noreply.github.com>
2025-07-21 17:25:31 +0530

    Update pwa.config.ts

--- pwa.config.ts
+++ pwa.config.ts
@@ -2,9 +2,9 @@ import type { Config } from './types';
 
 export default {
   id: '/',
-  name: 'My Blog',
-  shortName: 'My Blog',
-  description: 'My blog description goes here...',
+  name: 'MSBTE Scholar',
+  shortName: 'MSBTE Scholar',
+  description: 'MSBTE Scholar description goes here...',
   direction: 'auto',
   language: 'en-US',
   backgroundColor: '#fff',
@@ -39,5 +39,5 @@ export default {
     },
   },
   // Please replace with your blog url if you are using CDN (JsDelivr)
-  origin: 'https://hello-example.blogspot.com',
+  origin: 'https://materialuiwal.blogspot.com',
 } satisfies Config;
